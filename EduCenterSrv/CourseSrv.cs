ed71110a832d96fa16d63a32208fa2dd2f2cc9eb
JackySrv <song_fuwei@126.com>
2019-05-20 21:53:55 +0800

    1

--- EduCenterSrv/CourseSrv.cs
+++ EduCenterSrv/CourseSrv.cs
@@ -18,25 +18,13 @@ namespace EduCenterSrv
         public List<ECourseInfo> GetAllList()
         {
            return  _dbContext.Set<ECourseInfo>().ToList();
-         //  return _dbContext.DBCourseInfo.ToList();
+        
         }
 
         public ECourseInfo Get(string pk)
         {
             return _dbContext.DBCourseInfo.Where<ECourseInfo>(a => a.Code == pk).FirstOrDefault();
         }
-
-        //public void Add(ECourseInfo newObj)
-        //{
-        //    _dbContext.Add<ECourseInfo>(newObj);
-        //    _dbContext.SaveChanges();
-        //}
-
-        //public void Update(ECourseInfo Obj)
-        //{
-        //    _dbContext.Update<ECourseInfo>(Obj);
-        //    _dbContext.SaveChanges();
-        //}
 
       
 
