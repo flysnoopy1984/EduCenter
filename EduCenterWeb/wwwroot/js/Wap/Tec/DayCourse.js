c97be088e222e2349b0a2311cd53f425c3f7c0f3
flysnoopy1984 <song_fuwei@126.com>
2019-07-04 23:54:09 +0800

    home

--- EduCenterWeb/wwwroot/js/Wap/Tec/DayCourse.js
+++ EduCenterWeb/wwwroot/js/Wap/Tec/DayCourse.js
@@ -1,3 +1,22 @@
 ﻿$(function () {
+    Init = function () {
+        laydate.render({
+            elem: '.DateInput',
+            eventElem: '#btn_DatePick',
 
+            trigger: 'click',
+            theme: 'grid',
+            done: function (value, date) {
+                //   alert('你选择的日期是：' + value + '\n获得的对象是' + JSON.stringify(date));
+                QueryTecDayCourse(value);
+            }
+        });
+    }
+
+    QueryTecDayCourse = function (date) {
+
+    }
+
+    Init();
+  
 })
